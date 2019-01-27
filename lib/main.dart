783dca43d2fb6863abf600a3b9878cb5d0961d69
sochiai <sochiaig@gmail.com>
2019-01-27 21:28:10 +0900

    impliment navigator push pop

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/gestures.dart';
 
-import 'dart:typed_data';
-import 'dart:async';
-import 'dart:ui' as ui;
-import 'dart:math';
 
 void main() {
   runApp(new MyApp());
@@ -23,83 +16,86 @@ class MyApp extends StatelessWidget {
         accentColor: const Color(0xFF2196f3),
         canvasColor: const Color(0xFFfafafa),
       ),
-      home: new MyHomePage(),
+      home: new FirstScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  GlobalKey _homeStateKey = GlobalKey();
-  Offset _pos;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 255, 255, 255),
       appBar: AppBar(
-        title: Text('App Name'),
+        title: Text('Home'),
       ),
       body: Center(
-        child: Listener(
-          onPointerDown: _pointerDown,
-          onPointerMove: _pointerMove,
-          child: CustomPaint(
-            key: _homeStateKey,
-            painter: MyPainter(_pos),
-            child: ConstrainedBox(constraints: BoxConstraints.expand()),
+        child: Container(
+          child: Text('Home Screen',
+            style: TextStyle(
+              color: Colors.red,
+            ),
           ),
-        )
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            title: Text('Home'),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            title: Text('next'),
+            icon: Icon(Icons.navigate_next),
+          ),
+        ],
+        onTap: (int value) {
+          if(value == 1) {
+            Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondScreen()),
+            );
+          }
+        },
       ),
     );
   }
+}
 
-  void _pointerDown(PointerDownEvent event) {
-    RenderBox referenceBox = _homeStateKey.currentContext.findRenderObject();
-    setState(() {
-      _pos = referenceBox.globalToLocal(event.position);
-    });
-  }
-
-  void _pointerMove(PointerMoveEvent event) {
-    RenderBox referenceBox = _homeStateKey.currentContext.findRenderObject();
-    setState(() {
-      _pos = referenceBox.globalToLocal(event.position);
-    });
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Next'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Next Screen',
+            style: TextStyle(
+              color: Colors.red,
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            title: Text('prev'),
+            icon: Icon(Icons.navigate_before),
+          ),
+          BottomNavigationBarItem(
+            title: Text('?'),
+            icon: Icon(Icons.android),
+          ),
+        ],
+        onTap: (int value) {
+          if(value == 0) Navigator.pop(context);
+        },
+      ),
+    );
   }
 }
 
-class MyPainter extends CustomPainter {
-  Offset _pos;
-
-  MyPainter(this._pos);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    Paint p = Paint();
-    p.style = PaintingStyle.fill;
-    p.color = Color.fromARGB(25, 255, 0, 0);
-    if (_pos != null) {
-      for (var i = 0; i < 10; i++) {
-        canvas.drawCircle(_pos, 10.0 * i, p);
-      }
-      canvas.drawCircle(_pos, 50.0, p);
-    }
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => true;
-
-}
 
