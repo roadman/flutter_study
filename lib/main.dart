dc6c5297f13c1b9961965d687f5742edea18f24c
sochiai <sochiaig@gmail.com>
2019-01-27 19:30:29 +0900

    impliment hittest & handleevent

--- lib/main.dart
+++ lib/main.dart
@@ -60,57 +60,31 @@ class MyRenderBoxWidget extends SingleChildRenderObjectWidget {
 
 class _MyRenderBox extends RenderBox {
   ui.Image _img;
+  Offset _pos;
 
   @override
   bool hitTest(HitTestResult result, { @required Offset position }) {
+    result.add(BoxHitTestEntry(this, position));
     return true;
   }
 
-  _MyRenderBox() {
-    print("load image.");
-    loadAssetImage('image.jpg');
-  }
-
-  loadAssetImage(String fname) {
-    print("call loadAssetImage");
-    rootBundle.load
-      ("assets/$fname").then((bd) {
-      Uint8List u8lst = Uint8List.view(bd.buffer);
-      ui.instantiateImageCodec(u8lst).then((codec) {
-        codec.getNextFrame().then(
-                (frameInfo) {
-              _img = frameInfo.image;
-              markNeedsPaint();
-              print("_img created: $_img");
-            }
-        );
-      });
-    });
+  @override
+  void handleEvent(PointerEvent event, HitTestEntry entry) {
+    super.handleEvent(event, entry);
+    _pos = event.position;
+    markNeedsPaint();
   }
 
   @override
   void paint(PaintingContext context, Offset offset) {
     Canvas c = context.canvas;
-    double dx = offset.dx + 30.0;
-    double dy = offset.dy + 30.0;
-
-    if (_img != null) {
-      c.drawImage(_img, Offset(dx, dy), Paint());
-      print("draw img.");
-    } else {
-      print("_img is null.");
-    }
-
-    Paint p = Paint();
-    p.style = PaintingStyle.fill;
-
-    p.blendMode = BlendMode.darken;
-
-    for (var i = 0; i < 10; i++) {
-      for (var j = 0; j < 10; j++) {
-        p.color = Color.fromARGB(255, 25 * i, 0, 25 * i);
-        Rect r = Rect.fromLTWH(dx + 30.0 * i, dy + 30.0 * j, 30.0, 30.0);
-        c.drawOval(r, p);
+    c.drawColor(Colors.black, BlendMode.clear);
+    if(_pos != null) {
+      Paint p = Paint();
+      p.style = PaintingStyle.fill;
+      for (var i = 0; i < 10; i++) {
+        p.color = Color.fromARGB(50, 255, 255, 255);
+        c.drawCircle(_pos, i * 5.0, p);
       }
     }
   }
