5cd6f4afa4ca3d3a682173a59e09a55b67e2f80e
sochiai <sochiaig@gmail.com>
2019-01-27 23:29:05 +0900

    impliment navigation text send,recv

--- lib/main.dart
+++ lib/main.dart
@@ -21,21 +21,39 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
+  FirstScreen({Key key}) : super(key: key);
+
+  @override
+  _FirstScreenState createState() => new _FirstScreenState();
+}
+
+
+class _FirstScreenState extends State<FirstScreen> {
+  final _controller = TextEditingController();
+  String _input;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Center(
-        child: Container(
-          child: Text('Home Screen',
+      body: Column(
+        children: <Widget>[
+          Text('Home Screen',
             style: TextStyle(
               color: Colors.red,
-            ),
+            )
           ),
-        ),
+          Padding(
+            padding: EdgeInsets.all(10.0),
+          ),
+          TextField(
+            controller: _controller,
+            onChanged: changeField,
+          )
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 1,
@@ -53,16 +71,22 @@ class FirstScreen extends StatelessWidget {
           if(value == 1) {
             Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => SecondScreen()),
+                MaterialPageRoute(builder: (context) => SecondScreen(_input)),
             );
           }
         },
       ),
     );
   }
+
+  void changeField(String val) => _input = val;
 }
 
 class SecondScreen extends StatelessWidget {
+  final String _value;
+
+  SecondScreen(this._value);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -71,7 +95,8 @@ class SecondScreen extends StatelessWidget {
       ),
       body: Center(
         child: Container(
-          child: Text('Next Screen',
+          child: Text(
+            'you typed: "$_value".',
             style: TextStyle(
               color: Colors.red,
             ),
