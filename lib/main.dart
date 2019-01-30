1c55af847886acd0cc5e10645a700dc5f98419e4
sochiai <sochiaig@gmail.com>
2019-01-31 02:38:45 +0900

    use shared_Preference

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:path_provider/path_provider.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(new MyApp());
@@ -38,7 +35,15 @@ class FirstScreen extends StatefulWidget {
 
 class _FirstScreenState extends State<FirstScreen> {
   final _controller = TextEditingController();
-  final _fname = 'mydata.txt';
+  double _r = 0.0;
+  double _g = 0.0;
+  double _b = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    loadPref();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,50 +51,97 @@ class _FirstScreenState extends State<FirstScreen> {
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Column(
-        children: <Widget>[
-          Text('Home Screen',
-            style: TextStyle(
-              color: Colors.red,
-            )
-          ),
-          Padding( padding: EdgeInsets.all(20.0) ),
-          TextField(
-            maxLength: 5,
-            controller: _controller,
-          )
-        ],
+      body: Container(
+        color: Color.fromARGB(200, _r.toInt(), _g.toInt(), _b.toInt()),
+        child: Column(
+          children: <Widget>[
+            Text('Home Screen',
+              style: TextStyle(
+                color: Colors.red,
+              )
+            ),
+            Padding( padding: EdgeInsets.all(20.0) ),
+            TextField(
+              controller: _controller,
+            ),
+            Padding( padding: EdgeInsets.all(20.0) ),
+            Slider(
+              min: 0.0,
+              max: 255.0,
+              value: _r,
+              divisions: 255,
+              onChanged: (double value) {
+                setState(() {
+                  _r = value;
+                });
+              },
+            ),
+            Slider(
+              min: 0.0,
+              max: 255.0,
+              value: _g,
+              divisions: 255,
+              onChanged: (double value) {
+                setState(() {
+                  _g = value;
+                });
+              },
+            ),
+            Slider(
+              min: 0.0,
+              max: 255.0,
+              value: _b,
+              divisions: 255,
+              onChanged: (double value) {
+                setState(() {
+                  _b = value;
+                });
+              },
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.open_in_new),
         onPressed: () => setState(() {
-          loadIt().then((String value) {
-            setState(() {
-              _controller.text = value;
-            });
-            showDialog(
+          savePref();
+          showDialog(
               context: context,
               builder: (BuildContext context) => AlertDialog(
                 title: Text("loaded!"),
                 content: Text("load message from Asset."),
               )
-            );
-          });
+          );
         })
       ),
     );
   }
 
-  Future<String> getDataAsset(String path) async {
-    return await rootBundle.loadString(path);
+  void loadPref() async {
+    SharedPreferences.getInstance().then(
+      (SharedPreferences prefs) {
+        setState(() {
+          _r = (prefs.getDouble('r') ?? 0.0);
+          _g = (prefs.getDouble('g') ?? 0.0);
+          _b = (prefs.getDouble('b') ?? 0.0);
+          _controller.text = (prefs.getString('input') ?? '');
+        });
+      }
+    );
   }
 
-  Future<String> loadIt() async {
-    try {
-      return await getDataAsset(_fname);
-    } catch (e) {
-      return null;
-    }
+  void savePref() async {
+    SharedPreferences.getInstance().then(
+      (SharedPreferences prefs) {
+        setState(() {
+          prefs.setDouble('r', _r);
+          prefs.setDouble('g', _g);
+          prefs.setDouble('b', _b);
+          prefs.setString('input', _controller.text);
+        });
+      }
+    );
   }
+
 }
 
